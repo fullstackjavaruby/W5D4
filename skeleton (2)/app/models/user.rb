ff77511b0bb8bf0584c49aaefa763538b2d2f13e
Brian <fullstackjavaruby@gmail.com>
2021-05-20 20:35:54 -0400

    W5D4

--- skeleton (2)/app/models/user.rb
+++ skeleton (2)/app/models/user.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    # validates :name, presence: true, uniqueness: true
+
+    has_many :enrolled, 
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_course,
+        through: :enrolled,
+        source: :course
+
 end
---
+++ skeleton (2)/db/migrate/20210520210247_modify_users.rb
@@ -0,0 +1,4 @@
+class ModifyUsers < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
